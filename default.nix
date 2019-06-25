f0964aa360a8ae97f07ba3dde82a6c483d7c2d6c
Shay Bergmann <shay.bergmann@iohk.io>
2019-06-25 10:16:04 -0400

    Simplify
    previous modus operandi was:
    
    - copy terraform configs (generated or otherwise) into a store path
    - do some very hacky gymnastics to get terraform to run from that store
    path while writing all state to the original directory it was run from
    
    this works, but:
    - it's messy and hard to reason about
    - "regular" terraform modules can't access sources using relative paths
    
    Instead, we simply generate a single `nix.tf.json` store path from all `tf.nix` files in the current directory and link to it. So at the cost of having to `.gitignore` a file, we get to eliminate a bunch of messy code and support more use cases.

--- default.nix
+++ default.nix
@@ -1,112 +1,39 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {}
+, sources ? import ./nix/sources.nix }:
 
 let
-  # We are trying to wrap terraform as transparently as possible. To the user the only difference
-  # should be that it now takes `tf.nix` files as well.
-  # To achieve this: 
-  # - we convert `.tf.nix` files to `.tf.json` files,
-  # - we copy the resulting `.tf.json` files, along with any existing `.tf` and `.tf.json` files to a new derivation.
-  # 
-  # At this point all we have to do is manipulate Terraform to read the configs from the derivation we created
-  # While still writing the `.terraform` dir (which contains fetched plugins and modules) and the `terraform.tfstate` file to our working directory.
-  #
-  # Unfortunately, this is much more tricky than it should be. Terraform's interface isn't super consistent:
-  # - The Terraform configs directory can only be given as an optional cli argument to the subcommands that need it; defaults to current directory if not given.
-  #   We avoid having to intercept an optional argument simply by cd'ing into the derivation before passing control to Terraform.
-  #   This comes with the following caveats:
-  #   - If a directory is given as an argument, we basically fall back to "regular" Terraform. I can live with that.
-  #   - Since the nix store is read-only, we have must tell Terraform to write its state file and data directory back in our actual $PWD:
-  #     - The `.terraform` data dir: easy, it's controlled by an environment variable.
-  #     - The `terraform.tfstate` file, however, is controlled by a `-tfstate` cli option, which is again only taken by some subcommands
-  #       (and some sub-subcommands) that interact with the state, but not by others who do not: some of which would ignore it silently
-  #       while others would complain and error out:
-  #       - Terraform offers the `TF_CLI_ARGS`: it doesn't help us because of the above and because https://github.com/hashicorp/terraform/issues/14847
-  #         has been open since 2017. Next.
-  #       - Terraform also offers individual `TF_CLI_ARGS_command`: for example `TF_CLI_ARGS_plan="-tfstate=$PWD/terraform.tfstate"` would only add that flag
-  #         to `terraform plan`. This is where this whole background story is leading down to.
-  #         WE HAVE TO GO DOWN THE HARD, UGLY, SCREEN-SCRAPY WAY. Oh joy 🙃
+  niv = (import sources.niv {}).niv;
+  nur-packages = import sources.nur-packages {};
+  mkBashCli = pkgs.callPackage (sources.nur-packages + "/pkgs/make-bash-cli") { inherit (nur-packages.lib) grid; };
 
-  # This crawls `terraform -help` and outputs something like this:
-  # ```
-  # terraform commandA subcommand -optionA -optionB -optionC
-  # terraform commandB -optionA -optionD -optionE
-  # ... and so on
-  # ```
-  terraform-help-scraper = pkgs.writeShellScriptBin "terrascrape" ''
-    set -eo pipefail
-
-    PATH=${with pkgs; lib.makeBinPath [ coreutils findutils gnugrep gawk terraform ]}
-
-    if [[ -z $1 ]]; then
-      exec $0 index
-    fi
-
-    cmd=$1
-    shift
-
-    case $cmd in
-      get-cmds | gc)
-        exec "$@" -help | (egrep '^ {4}[[:alnum:]][[:alnum:]-]' || true) | awk '{ print $1 }' | xargs --no-run-if-empty --max-args=1 echo "$@" | tee >(cat 1>&2)
-        ;;
-      get-cmds-recursive | gcr)
-        exec $0 get-cmds "$@" | xargs --no-run-if-empty --max-args=$(( $# + 1 )) $0 get-cmds-recursive
-        ;;
-      get-opts | go)
-        (echo $@ ; $@ -help | (egrep -- '^ {2,4}-' || true) | awk '{ print $1 }' | cut -d= -f1) | xargs
-        ;;
-      index | idx | i)
-        exec $0 get-cmds-recursive terraform 2>&1 | xargs --no-run-if-empty --max-args=1 -d '\n' $0 get-opts
-        ;;
-      *)
-        1>&2 echo "idk wtf you want. kthxbye."
-        exit 1
-        ;;
-    esac
-  '';
-
-  # Here 👇 we save the output from there ☝️   because it's kinda expensive to generate.
-  terraform-option-index = pkgs.runCommand "terraform-option-index" {
-    buildInputs = [ terraform-help-scraper ];
-  } ''
-    #            👇 lose the first 'terraform' column as it's pretty uselesss
-    terrascrape | cut -d' ' -f2- > $out
-  '';
-  
   wrapped-terraform = pkgs.writeShellScriptBin "terraform" ''
-    set -eo pipefail
-
-    PATH=${with pkgs; lib.makeBinPath [ coreutils findutils gnugrep gnused nix terraform ]}
-
-    result=$(nix-build ${./.}/eval.nix --show-trace --no-out-link --attr terraform.result)
-
-    export TF_DATA_DIR=$PWD/.terraform
-    # 👇 here we use it to set `-state` only for the commands and sub-commands that care for it.
-    eval $(cat ${terraform-option-index} | egrep -- '-state([[:space:]]|$)' | sed -E 's/ -.*$//' | tr ' ' _ | xargs -I'%' echo "export TF_CLI_ARGS_%='-state=$PWD/terraform.tfstate'")
-
-    cd $result
-    exec terraform "$@"
+    exec jw terraform "$@"
   '';
 
-  attribute-tester = pkgs.writeShellScriptBin "terrattr" ''
-    PATH=${with pkgs; lib.makeBinPath [ nix ]}
-    attr=''${1?Must specify attribute}
-    shift
-    nix-instantiate --show-trace --eval --strict "$@" ${./.}/eval.nix --attr terraform.$attr
-  '';
+  cli = mkBashCli "jw" "generate terraform configs with nix" {} (mkCmd: [
+    (mkCmd "eval" "eval *.tf.nix files and dump a nix.tf.json." {} ''
+      PATH=${with pkgs; lib.makeBinPath [ nix ]}:$PATH
+      nix-build ${./.}/eval.nix --show-trace --out-link nix.tf.json --attr terraform.result
+    '')
+    (mkCmd "terraform" "eval and handover control to terraform." {
+      aliases = [ "tf" ];
+    } ''
+      $0 eval
+      PATH=${with pkgs; lib.makeBinPath [ terraform ]}:$PATH
+      exec terraform "$@"
+    '')
+    (mkCmd "init" "setup new project" {
+      aliases = [ "i" ];
+    } ''
+      PATH=${with pkgs; lib.makeBinPath [ niv ]}:$PATH
+      niv init
+      niv drop jack-williamson || true
+      niv add --branch simplify kreisys/jack-williamson
+      install -m 644 ${./shell.nix} shell.nix
+      echo use nix > .envrc
+    '')
+  ]);
 
-  attribute-builder = pkgs.writeShellScriptBin "terrabuild" ''
-    PATH=${with pkgs; lib.makeBinPath [ nix ]}
-    attr=''${1?Must specify attribute}
-    shift
-    nix-build ${./.}/eval.nix --attr terraform.$attr
-  '';
-
-in pkgs.buildEnv {
-  name  = "jack-williamson";
-  paths = [
-    attribute-tester
-    attribute-builder
-    terraform-help-scraper
-    wrapped-terraform
-  ];
+in {
+  inherit cli wrapped-terraform;
 }
--- modules/terraform.nix
+++ modules/terraform.nix
@@ -3,60 +3,21 @@
 let
   cfg = config.terraform;
 
-  is       = type: path: (lib.pathType path) == type;
-  isDir    = is "directory"; # ?
-  isFile   = is   "regular"; # file ?
-
-  isTfFile     = lib.hasSuffix ".tf";
-  isTfNixFile  = lib.hasSuffix ".tf.nix";
-  isTfJsonFile = lib.hasSuffix ".tf.json";
-
 in {
   options.terraform = with lib; with types; let
-    dir        = addCheck path isDir        // { name =        "dir"; description = "directory"; };
-    file       = addCheck path isFile       // { name =       "file"; description = "regular file"; };
-    tfFile     = addCheck file isTfFile     // { name =     "tfFile"; description = "terraform file"; };
-    tfNixFile  = addCheck file isTfNixFile  // { name =  "tfNixfile"; description = "terraform nix file"; };
-    tfJsonFile = addCheck file isTfJsonFile // { name = "tfJsonfile"; description = "terraform json file"; };
 
   in {
     dir = mkOption {
-      type = nullOr path;
+      type = nullOr str;
 
-      description = ''
-        A directory containing a Terraform config from which we will generate a derivation where:
-        - <filename>*.tf.nix</filename> files will be:
-          - evaluated alongside whatever might be set in <option>config</option> (empty set by default)
-          - converted into a single <filename>terraform.tf.json</filename> file.
-        - Everything else will be copied <literal>as-is</literal>.
-        
-        That means that any filtering (<filename>terraform.tfstate*</filename> and <filename>.terraform/</filename> come to mind) is the
-        responsibility of the consumer and not a the concern of this module. The rationale here, if we take <filename>terraform.tfstate</filename> as an
-        example, is that we cannot predict with certainty where it's going to be at this point; as it may be overridden by a CLI arg
-        given to the `terraform` executable. Therefore the supplied <filename>terraform</filename> wrapper, is a better candidate for this
-        particular concern.
+      description = "Directory with tf.nix files";
 
-        The default, which takes the current working directory and filters out <filename>.terraform/</filename> and <filename>.terraform.tfstate*</filename>
-        is meant as an example.
-
-        This may also be set to <literal>null</literal>, in which case, only configuration in <option>config</option> will be used.
-      '';
-
-      default = cleanSourceWith {
-        src    = cleanSource (builtins.getEnv "PWD");
-        filter = name: type: let
-          baseName = baseNameOf (toString name);
-        in !(
-          ((baseName == ".terraform")                 && (type == "directory")) || 
-          ((hasPrefix   "terraform.tfstate" baseName) && (type == "regular"))
-        );
-      };
-
-      defaultText = "CWD";
+      default = builtins.getEnv "PWD";
+      defaultText = "$PWD";
     };
 
     tfNixFiles = mkOption {
-      type     = listOf path;
+      type     = listOf str;
       internal = true;
     };
 
@@ -94,7 +55,7 @@ in {
           };
 
           terraform = mkOption {
-            type    = nullOr attrs;
+            type    = nullOr (attrsOf (either str (attrsOf (either str attrs))));
             default = null;
           };
 
@@ -133,15 +94,6 @@ in {
 
     configTfJson = builtins.toJSON (lib.filterAttrs (name: value: name != "_module" && value != null) cfg.config);
 
-    result = with lib; pkgs.runCommand "terraform-${baseNameOf (builtins.getEnv "PWD")}" {
-      inherit (cfg) configTfJson;
-      src         = cfg.dir;
-      passAsFile  = [ "configTfJson" ];
-      buildInputs = [ pkgs.terraform ];
-    } ''
-      cp -r ''${src:-/var/empty} $out
-      chmod +w $out
-      install --mode 444 $configTfJsonPath $_/terraform.tf.json
-    '';
+    result = pkgs.writeText "nix.tf.json" cfg.configTfJson;
   };
 }
